928d956fe5d38cbcfe75d5e28a132fcc1f27e416
Willian Pacheco Silva <willianpacheco31@gmail.com>
2024-01-25 17:18:18 -0300

    feat: adding imports to finetune study

--- studies/finetune.py
+++ studies/finetune.py
@@ -1,1 +1,5 @@
+import torch
+from torch.utils.data import Dataset
+from transformers import DistilBertTokenizerFast, DistilBertForSequenceClassification, Trainer, TrainingArguments
+
 model_name = "distilbert-base-uncased"
