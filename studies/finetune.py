99e93334d759089763b47b93159001253940fa28
Willian Pacheco Silva <willianpacheco31@gmail.com>
2024-01-25 17:29:37 -0300

    feat: creating function to read dataset

--- studies/finetune.py
+++ studies/finetune.py
@@ -8,3 +8,12 @@ model_name = "distilbert-base-uncased"
 
 def read_imdb_split(split_dir):
     split_dir = Path(split_dir)
+    texts = []
+    labels = []
+
+    for label_dir in ["pos", "neg"]:
+        for text_file in (split_dir/label_dir).iterdir():
+            texts.append(text_file.read_text())
+            labels.append(0 if label_dir == "neg" else 1)
+    
+    return texts, labels
