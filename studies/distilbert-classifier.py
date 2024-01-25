70f1b03f6374131933ff2a5aca76508fc7dbb8c1
Willian Pacheco Silva <willianpacheco31@gmail.com>
2024-01-25 16:59:31 -0300

    feat: loading model and tokenizer

--- studies/distilbert-classifier.py
+++ studies/distilbert-classifier.py
@@ -5,7 +5,7 @@ model_name = "distilbert-base-uncased-finetuned-sst-2-english"
 model = AutoModelForSequenceClassification.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-classifier = pipeline("sentiment-analysis")
+classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
 
 results = classifier(["I am very happy!", "I am sad!"])
 
