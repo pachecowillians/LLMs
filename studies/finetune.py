7b7ba440b77fa4821ec44a4547a62b1b41086172
Willian Pacheco Silva <willianpacheco31@gmail.com>
2024-01-25 17:23:18 -0300

    feat: updating requirements

--- studies/finetune.py
+++ studies/finetune.py
@@ -1,5 +1,10 @@
 import torch
+from pathlib import Path
 from torch.utils.data import Dataset
+from sklearn.model_selection import train_test_split
 from transformers import DistilBertTokenizerFast, DistilBertForSequenceClassification, Trainer, TrainingArguments
 
 model_name = "distilbert-base-uncased"
+
+def read_imdb_split(split_dir):
+    split_dir = Path(split_dir)
