15e33c80124ceff31b2facef71908cd9b4e38cda
Willian Pacheco Silva <willianpacheco31@gmail.com>
2024-01-25 18:57:03 -0300

    feat: finetuning model

--- studies/finetune.py
+++ studies/finetune.py
@@ -17,3 +17,56 @@ def read_imdb_split(split_dir):
             labels.append(0 if label_dir == "neg" else 1)
     
     return texts, labels
+
+# https://ai.stanford.edu/~amaas/data/sentiment/
+
+train_texts, train_labels = read_imdb_split("./studies/aclImdb/train")
+test_texts, test_labels = read_imdb_split("./studies/aclImdb/test")
+
+train_texts, val_texts, train_labels, val_labels = train_test_split(train_texts, train_labels, test_size=0.2)
+
+class IMDBDataset(Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, index):
+        item = {key: torch.tensor(val[index]) for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels[index])
+        return item
+    
+    def __len__(self):
+        return len(self.labels)
+    
+tokenizer = DistilBertTokenizerFast.from_pretrained(model_name)
+
+train_encodings = tokenizer(train_texts, truncation=True, padding=True)
+val_encodings = tokenizer(val_texts, truncation=True, padding=True)
+test_encodings = tokenizer(test_texts, truncation=True, padding=True)
+
+train_dataset = IMDBDataset(train_encodings, train_labels)
+val_dataset = IMDBDataset(val_encodings, val_labels)
+test_dataset = IMDBDataset(test_encodings, test_labels)
+
+training_args = TrainingArguments(
+    output_dir="./results",
+    num_train_epochs=2,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=64, 
+    warmup_steps=500,
+    learning_rate=5e-5,
+    weight_decay=0.01,
+    logging_dir="./logs",
+    logging_steps=10,
+)
+
+model = DistilBertForSequenceClassification.from_pretrained(model_name)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+    eval_dataset=val_dataset,
+)
+
+trainer.train()
